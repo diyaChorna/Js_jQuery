997740f3811ae13e72f55e41c87b6a1456e04728
unknown <diana.chorna@gmail.com>
2022-01-23 21:40:06 +0200

    2

---
+++ 2/main.js
@@ -0,0 +1,38 @@
+// alert(`fjg`);
+// alert(`two`);
+
+// $('.input').blur(function(){
+//     let val = $(this).val();
+//     alert(val);
+// })
+
+// let html = $('.input').html();
+// // console.log(html);
+// alert(html)
+
+
+
+
+// 1
+// let s1 = $('#s1').html();
+// alert(s1);
+
+// //2
+// let s2 = $('.s2').html();
+// alert(s2);
+
+//3
+// let second = $('#second i').html();
+// alert(second);
+
+// //4
+// let q = $('[title="hello"]').html()
+// alert(q);
+
+// //5
+// let a = $('[target="_blank"]').html()
+// alert(a);
+
+//6
+// let extra = $('.extra:eq(1)').html();
+// alert(extra)
